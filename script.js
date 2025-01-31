eef4397dbaf4122d3debfeff4e4c5fa6d275ed83
Rubleenkaur1313 <rubleenkaur05@gmail.com>
2025-01-31 21:12:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+    / Get the table by its ID
+    var table = document.getElementById("sampleTable");
+    
+    // Create a new row
+    var newRow = table.insertRow();
+
+    // Create a new cell for the left side
+    var leftCell = newRow.insertCell(0);
+    leftCell.innerHTML = "New Cell1"; // Set the value for the left cell
+
+    // Create a new cell for the right side
+    var rightCell = newRow.insertCell(1);
+    rightCell.innerHTML = "New Cell2"; // Set the value for the right cell
 }
+
